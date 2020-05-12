3c56021fe0f1a44dc8a77e1b22759d263279bd62
Anna Tsolakou <anna.tsolakou@amadeus.com>
2020-05-12 10:52:52 +0200

    update poi class

--- amadeus_demo/map/point_of_interest.py
+++ amadeus_demo/map/point_of_interest.py
@@ -1,4 +1,5 @@
 class PointOfInterest:
+    
     def __init__(self, poi):
         self.poi = poi
 
@@ -6,6 +7,9 @@ class PointOfInterest:
         try:
             place = {}
             place['name'] = self.poi['name']
+            place['category'] = self.poi['category']
+            place['lat'] = self.poi['geoCode']['latitude']
+            place['lng'] = self.poi['geoCode']['longitude']
         except (TypeError, AttributeError, KeyError):
             pass
         return place
