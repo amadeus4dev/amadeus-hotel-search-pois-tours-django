bd504f43c17fcfdc16bae4e265fb0a0ebcbf7006
Anna Tsolakou <anna.tsolakou@amadeus.com>
2024-04-10 16:07:12 +0200

    fix hotel issue on the map

--- amadeus_demo/map/urls.py
+++ amadeus_demo/map/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.hotels_map, name='hotels_map'),
     path('search_pois/', views.search_pois, name='search_pois'),
-    path('search_activity/', views.search_activity, name='search_activity'),
+    path('search_activity/', views.search_activity, name='search_activity')
 
 ]
